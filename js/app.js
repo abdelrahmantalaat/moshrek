f75f1c6fb23d1dadd8b1e11d828845d69c612258
abdelrahmantalaat <abdelrhmanemara2@gmail.com>
2021-09-04 16:44:29 +0200

    finished

--- js/app.js
+++ js/app.js
@@ -3,22 +3,36 @@ window.onscroll = function() {
     nav.style.backgroundColor = '#F5F7FE'
 };
 
+
+        
 $(document).ready(function(){
     $("#player-next-btn").click(function(){
-        $("#player-info").fadeOut()
+        $("#player-info").fadeOut("fast")
         $("#academy-info").fadeIn("slow")
+        $("#first-sec").removeClass("current-sec")
+        $("#secound-sec").addClass("current-sec")
+       
     })
     $("#academy-next-btn").click(function(){
-        $("#academy-info").fadeOut()
+        $("#academy-info").fadeOut("fast")
         $("#coach-info").fadeIn("slow")
+        $("#secound-sec").removeClass("current-sec")
+        $("#third-sec").addClass("current-sec")
+   
     })
     $("#academy-prev-btn").click(function(){
-        $("#academy-info").fadeOut()
+        $("#academy-info").fadeOut("fast")
         $("#player-info").fadeIn("slow")
+        $("#secound-sec").removeClass("current-sec")
+        $("#first-sec").addClass("current-sec")
+  
     })
     $("#coach-prev-btn").click(function(){
-        $("#coach-info").fadeOut()
+        $("#coach-info").fadeOut("fast")
         $("#academy-info").fadeIn("slow")
+        $("#third-sec").removeClass("current-sec")
+        $("#secound-sec").addClass("current-sec")
+   
     })
     });
 
